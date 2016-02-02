c77de0fc792b79decd2586199ecc578dafd9192f
samir7575 <moulik_6@live.fr>
2016-02-02 23:15:55 +0100

    Commit du commentaire du repository du client.

--- spring-boot-mysql-springdatajpa-hibernate/src/main/java/com/softdesign/dao/ClientDAO.java
+++ spring-boot-mysql-springdatajpa-hibernate/src/main/java/com/softdesign/dao/ClientDAO.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.softdesign.domain.Client;
 
+/**
+ * Repository des clients
+ * @author malek
+ *
+ */
 public interface ClientDAO extends CrudRepository<Client, Long>
 {
 
